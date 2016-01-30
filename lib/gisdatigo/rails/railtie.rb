b658250d2a0a8992b745efddcbd5f8485d0deca3
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2016-01-30 17:35:24 -0200

    Mark railtie to not track test coverage
    It is just one line that would require a entire Rails app to get tested.
    It's not worth the cost.

--- lib/gisdatigo/rails/railtie.rb
+++ lib/gisdatigo/rails/railtie.rb
@@ -4,10 +4,13 @@ require 'rails'
 module Gisdatigo
   module Rails
     class Railtie < ::Rails::Railtie
-      # FIXME: We habe no tests here
+      # Testing this require to load it inside a rails app
+      # So this unique line is not worth the cost so far
+      # :nocov:
       rake_tasks do
         load "tasks/gisdatigo.rake"
       end
+      # :nocov:
     end
   end
 end
