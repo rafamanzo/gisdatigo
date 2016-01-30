14b65b8f92f78d0a92edd06615d7e34f5a9fdc3b
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2016-01-30 19:33:34 -0200

    Add configuration structure
    This will enable to keep configurations and load them through files.
    
    The first one is the test_command.

--- lib/gisdatigo.rb
+++ lib/gisdatigo.rb
@@ -6,4 +6,34 @@ require "gisdatigo/test_manager"
 require "gisdatigo/runner"
 require "gisdatigo/rails/railtie"
 
-module Gisdatigo; end
+module Gisdatigo
+  @configurations = {
+    test_command: 'rake'
+  }
+  @valid_configuration_keys = @configurations.keys
+
+  # Configure through Hash
+  def Gisdatigo.configure(opts = {})
+    opts.each do |key,value|
+      if @valid_configuration_keys.include? key.to_sym
+        @configurations[key.to_sym] = value
+      end
+    end
+  end
+
+  # Configure through yaml file
+  def Gisdatigo.configure_with(path_to_yaml_file)
+    begin
+      config = YAML.load_file(path_to_yaml_file)
+      configure(config)
+    rescue Errno::ENOENT
+      warn "YAML configuration file couldn't be found. Using defaults."
+    rescue Psych::SyntaxError
+      warn "YAML configuration file contains invalid syntax. Using defaults."
+    end
+  end
+
+  def Gisdatigo.configurations
+    @configurations
+  end
+end
--- spec/gisdatigo_spec.rb
+++ spec/gisdatigo_spec.rb
@@ -4,4 +4,80 @@ describe Gisdatigo do
   it 'has a version number' do
     expect(Gisdatigo::VERSION).not_to be nil
   end
+
+  describe 'configuration' do
+    describe 'configurations' do
+      it 'is expected to return the configurations Hash' do
+        expect(Gisdatigo.configurations[:test_command]).to_not be_nil
+      end
+    end
+
+    describe 'configure' do
+      context 'with a valid configuration' do
+        let(:configuration) { {test_command: 'test'} }
+
+        it 'is expected to set the configuration value' do
+          Gisdatigo.configure configuration
+
+          expect(Gisdatigo.configurations[:test_command]).to eq configuration[:test_command]
+        end
+      end
+
+      context 'with an invalid configuration' do
+        let(:configuration) { {not_valid: 'test'} }
+
+        it 'is expected to keep the default' do
+          previous = Gisdatigo.configurations
+
+          Gisdatigo.configure configuration
+
+          expect(Gisdatigo.configurations).to eq previous
+        end
+      end
+    end
+
+    describe 'configure_with' do
+      let(:file_path){ 'file' }
+
+      context 'with a existent file' do
+        context 'with correct synthax' do
+          let!(:loaded_file){ mock('loaded_file') }
+
+          before :each do
+            YAML.expects(:load_file).with(file_path).returns(loaded_file)
+          end
+
+          it 'is expected to call the configure method' do
+            Gisdatigo.expects(:configure).with(loaded_file)
+
+            Gisdatigo.configure_with(file_path)
+          end
+        end
+
+        context 'with broken synthax' do
+          before :each do
+            YAML.expects(:load_file).with(file_path).raises(Psych::SyntaxError.new(0, 0, 0, 0, 0, 0))
+          end
+
+          it 'is expected to print a warn and keep the defaults' do
+            Gisdatigo.expects(:warn).with("YAML configuration file contains invalid syntax. Using defaults.")
+
+            Gisdatigo.configure_with(file_path)
+          end
+        end
+      end
+
+      context 'with an absent file' do
+        before :each do
+          YAML.expects(:load_file).with(file_path).raises(Errno::ENOENT)
+        end
+
+        it 'is expected to print a warn and keep the defaults' do
+          Gisdatigo.expects(:warn).with("YAML configuration file couldn't be found. Using defaults.")
+
+          Gisdatigo.configure_with(file_path)
+        end
+      end
+    end
+  end
 end
