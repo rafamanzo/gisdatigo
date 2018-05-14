ad158ab46c310a2da38cd53c7424c2f31216d9bb
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2018-05-13 22:05:35 -0300

    Refactor Runner::Updater into smaller methods
    This helps on code reuse when implementing multiple update steps with
    different bundler parameters.

--- lib/gisdatigo/runner/updater.rb
+++ lib/gisdatigo/runner/updater.rb
@@ -3,27 +3,36 @@ module Gisdatigo
     class Updater < Base
       def update
         puts "Running updates:"
+        outdated_list = fetch_outdated_list
+        update_gems_with_options(outdated_list, ['--conservative'])
+      end
 
-        print "\tFetching outdated list..."
-        outdated_list = BundlerManager.gem_name_list
-        print "OK!\n"
+      private
 
+      def update_gems_with_options(outdated_list, options)
         current_package = 1
+
         outdated_list.each do |gem_name|
           puts "\t#{current_package}/#{outdated_list.count} - Updating #{gem_name}"
-          BundlerManager.update_gem(gem_name, ['--conservative'])
+          BundlerManager.update_gem(gem_name, options)
           commit(gem_name)
           current_package = current_package + 1
         end
       end
 
-      private
+      def fetch_outdated_list
+        print "\tFetching outdated list..."
+        outdated_list = BundlerManager.gem_name_list
+        print "OK!\n"
+
+        outdated_list
+      end
 
       def commit(gem_name)
-          if @git_manager.has_changes?
-            test("ERROR: Failed to update #{gem_name}! Tests were broken!")
-            @git_manager.commit(gem_name)
-          end
+        if @git_manager.has_changes?
+          test("ERROR: Failed to update #{gem_name}! Tests were broken!")
+          @git_manager.commit(gem_name)
+        end
       end
     end
   end
--- spec/runner/updater_spec.rb
+++ spec/runner/updater_spec.rb
@@ -1,28 +1,61 @@
 require 'spec_helper'
 
 describe Gisdatigo::Runner::Updater do
-  describe 'methods' do
-    let!(:git_manager) { mock('git_manager') }
+  let!(:git_manager) { mock('git_manager') }
 
-    before :each do
-      Gisdatigo::GitManager.expects(:new).returns(git_manager)
+  before :each do
+    Gisdatigo::GitManager.expects(:new).returns(git_manager)
+  end
+
+  describe 'methods' do
+    describe 'update' do
+      let(:outdated_gems_list) { mock('outdated_gems_list') }
+      let(:conservative_options) { ['--conservative'] }
+
+      it 'is expected to retrieve the outdated list and try to update' do
+        subject.expects(:fetch_outdated_list).returns(outdated_gems_list)
+        subject.expects(:update_gems_with_options).with(outdated_gems_list, conservative_options)
+
+        expect { subject.update }.to output("Running updates:\n").to_stdout
+      end
+    end
+  end
+
+  describe 'private method' do
+
+    describe 'update_gems_with_options' do
+      let(:outdated_gem_name){ 'outdated1' }
+      let(:outdated_gems_list) { [outdated_gem_name] }
+      let(:options) { [] }
+
+      it 'is expected to call the BundlerManager#update, print status and commit' do
+        expected_out = "\t1/1 - Updating #{outdated_gem_name}\n"
+        subject.expects(:commit).with(outdated_gem_name)
+
+        expect { subject.send(:update_gems_with_options, outdated_gems_list, options) }.to output(expected_out).to_stdout
+      end
     end
 
-    describe 'update' do
-      subject { Gisdatigo::Runner::Updater.new }
-      let(:outdated_gem_name){ 'outdated1' }
+    describe 'commit' do
+      let(:gem_name) { 'outdated42' }
 
-      it 'is expected to retrieve the outdated list, try to update them and commit the changes printing the status' do
-        Gisdatigo::BundlerManager.expects(:gem_name_list).returns([outdated_gem_name])
-        subject.expects(:test).with("ERROR: Failed to update #{outdated_gem_name}! Tests were broken!")
+      it 'is expected to check for changes, test them and commit' do
+        subject.expects(:test).with("ERROR: Failed to update #{gem_name}! Tests were broken!")
         git_manager.expects(:has_changes?).returns(true)
-        git_manager.expects(:commit).with(outdated_gem_name)
+        git_manager.expects(:commit).with(gem_name)
 
-        expected_out = "Running updates:\n"
-        expected_out << "\tFetching outdated list...OK!\n"
-        expected_out << "\t1/1 - Updating #{outdated_gem_name}\n"
+        subject.send(:commit, gem_name)
+      end
+    end
 
-        expect { subject.update }.to output(expected_out).to_stdout
+    describe 'fetch_outdated_list' do
+      it 'is expected to invoke the BundlerManager#gem_name_list' do
+        outdated_gems_list = mock('outdated_gems_list')
+        Gisdatigo::BundlerManager.expects(:gem_name_list).returns(outdated_gems_list)
+
+        expected_out = "\tFetching outdated list...OK!\n"
+
+        expect { expect(subject.send(:fetch_outdated_list)).to eq(outdated_gems_list) }.to output(expected_out).to_stdout
       end
     end
   end
